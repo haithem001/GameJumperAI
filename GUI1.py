96eac9dbcf5e2975363ce15ae1ac168c1dcdd5e8
onfire <haithemtrabelsino@gmail.com>
2024-02-19 12:33:00 +0100

    Fix left corner tile 1 collision

--- GUI1.py
+++ GUI1.py
@@ -3,7 +3,7 @@ from enum import Enum
 
 BLACK = (0, 0, 0)
 White = (255, 255, 255)
-
+Red = (255,0,0)
 
 
 class Direction(Enum):
@@ -14,14 +14,15 @@ class Direction(Enum):
 
 
 class Game:
-    def set_text(string, coordx, coordy, fontSize): #Function to set text
+    def set_text(self,string, coordx, coordy, fontSize):  # Function to set text
 
-        font = pygame.font.Font('freesansbold.ttf', fontSize)
-        #(0, 0, 0) is black, to make black text
-        text = font.render(string, True, (0, 0, 0))
+        font = pygame.font.Font('04B_30__.TTF', fontSize)
+        # (0, 0, 0) is black, to make black text
+        text = font.render(string, True, Red)
         textRect = text.get_rect()
         textRect.center = (coordx, coordy)
         return (text, textRect)
+
     def __init__(self, w=900, h=900):
         self.w = w
         self.h = h
@@ -31,50 +32,45 @@ class Game:
         self.direction = Direction.FIX
         self.D = pygame.Rect(self.w / 2, self.h - 200, 60, 60)
 
-
-
-
-        self.ListOfThem=[pygame.Rect(self.w / 2 + 100, self.h - 300, 100, 10),
-                         pygame.Rect(self.w / 2 - 200, self.h - 400, 100, 10),
-                         pygame.Rect(self.w - 30, self.h - 400, 100, 10),
-                         pygame.Rect(self.w / 2 + 100, self.h - 595, 100, 10),
-                         pygame.Rect(self.w / 2 - 200, self.h - 530, 100, 10),
-                         pygame.Rect(600, self.h - 700, 100, 10),
-                         pygame.Rect(400, self.h - 700, 100, 10),
-                         pygame.Rect(self.w - 30, self.h - 800, 100, 10),
-                         pygame.Rect(0, self.h - 700, 100, 10),
-                         pygame.Rect(0, self.h - 450, 100, 10),
-                         pygame.Rect(self.w/2-20, self.h - 140,100, 10)]
+        self.ListOfThem = [pygame.Rect(self.w / 2 + 100, self.h - 300, 100, 10),
+                           pygame.Rect(self.w / 2 - 200, self.h - 400, 100, 10),
+                           pygame.Rect(self.w - 30, self.h - 400, 100, 10),
+                           pygame.Rect(self.w / 2 + 100, self.h - 595, 100, 10),
+                           pygame.Rect(self.w / 2 - 200, self.h - 530, 100, 10),
+                           pygame.Rect(600, self.h - 700, 100, 10),
+                           pygame.Rect(400, self.h - 700, 100, 10),
+                           pygame.Rect(self.w - 30, self.h - 800, 100, 10),
+                           pygame.Rect(0, self.h - 700, 100, 10),
+                           pygame.Rect(0, self.h - 450, 100, 10),
+                           pygame.Rect(self.w / 2 - 20, self.h - 140, 100, 10)]
 
         self.velocity = 0
         self.on_ground = False
-        self.is_jumping=False
+        self.is_jumping = False
         self.jump_height = 15
 
-
-    def Collisions(self , Tile):
+    def Collisions(self, Tile):
         if (self.D.x < Tile.x + Tile.w and
                 self.D.x + self.D.w > Tile.x and
-                self.D.y < Tile.y + 1.5*Tile.h and
+                self.D.y < Tile.y + 1.5 * Tile.h and
                 self.D.y + self.D.h > Tile.y):
             self.velocity = 0
-            self.is_jumping=True
+            self.is_jumping = True
 
-            if (Tile.y-self.D.y<self.D.h) and not (Tile.x-55 < self.D.x ):
-                self.D.x = Tile.x-self.D.w
+            if (Tile.y - self.D.y < self.D.h) and not (Tile.x - 55 < self.D.x):
+                self.D.x = Tile.x - self.D.w
 
-            elif (Tile.y-self.D.y<self.D.h) and not (Tile.x > self.D.x -95 ):
-                self.D.x = Tile.x+Tile.w
+            elif (Tile.y - self.D.y < self.D.h) and not (Tile.x > self.D.x - 95):
+                self.D.x = Tile.x + Tile.w
 
             elif (self.D.y + self.D.h >= Tile.y) and (self.D.y < Tile.y):
                 self.D.y = Tile.y - self.D.h
-                self.is_jumping=False
+                self.is_jumping = False
             else:
                 self.on_ground = False
                 if self.D.y > self.h - 200:
                     self.velocity += 0.5  # Increase velocity due to gravity
                     self.D.y += self.velocity
-
 
     def play_step(self):
         for event in pygame.event.get():
@@ -87,28 +83,23 @@ class Game:
                 elif event.key == pygame.K_RIGHT:
                     self.direction = Direction.RIGHT
                 elif event.key == pygame.K_UP and not self.is_jumping:
-                        self.is_jumping=True
-                        self.velocity = -self.jump_height
-                        self.on_ground = False
-
-
-
+                    self.is_jumping = True
+                    self.velocity = -self.jump_height
+                    self.on_ground = False
 
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                     self.direction = Direction.FIX
-                elif event.key == pygame.K_UP:
-                    self.velocity = 0
-                    self.on_ground = False
+
 
         self._move()
         self._update_ui()
 
     def reset(self):
-        self.D.x=self.w/2
-        self.D.y=self.h - 180-self.D.h
+        self.D.x = self.w / 2
+        self.D.y = self.h - 180 - self.D.h
 
-#MOOD
+    # MOOD
     def _move(self):
         if self.direction == Direction.RIGHT:
             self.D.x += 5
@@ -122,7 +113,7 @@ class Game:
             for i in self.ListOfThem:
                 self.Collisions(i)
 
-        if self.D.y >= self.h : # Check if the character has landed on the ground
+        if self.D.y >= self.h:  # Check if the character has landed on the ground
             self.reset()
 
         # Keep the character within the game boundaries
@@ -135,8 +126,8 @@ class Game:
         self.display.fill(BLACK)
         for i in self.ListOfThem:
             pygame.draw.rect(self.display, White, i)
-
-
+        Show = self.set_text("MOTHER FUCKER", self.w/2, 40, 20)
+        self.display.blit(Show[0], Show[1])
 
         pygame.draw.rect(self.display, White, self.D)
 
